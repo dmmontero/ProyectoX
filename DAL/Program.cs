9a63d4f41523e225db5e62395445c021a71737e8
dmmontero <danny.montero@gmail.com>
2019-01-31 17:54:13 -0500

    Inserción datos bd

--- DAL/Program.cs
+++ DAL/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.Modelos;
+using System;
 
 namespace DAL
 {
@@ -6,7 +7,29 @@ namespace DAL
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var context = new SchoolContext())
+            {
+                var grade = new Grade()
+                {
+                    GradeName = "Grade 1",
+                    Section = "One"
+                };
+
+                //context.Grades.Add(grade);
+
+                var std = new Student()
+                {
+                    StudentName = "Danny Mauricio Montero Valencia",
+                    Height = 1.73m,
+                    Weight = 70.0f,
+                    DateOfBirth = new System.DateTime(1975, 3, 15),
+                    Grade = grade
+                    //Grade = context.Grades.Find(1)
+                };
+
+                context.Students.Add(std);
+                context.SaveChanges();
+            }
         }
     }
 }
